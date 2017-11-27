e7e1a4441ddd984f838fd135e6aefc8248467ef3
Paul Ionescu <ionepaul76@gmail.com>
2017-11-27 21:57:34 +0200

    hire us form integrated and contact changed styles

--- HorseSpot.Web/ClientApp/app/hire-us/hire-us.component.ts
+++ HorseSpot.Web/ClientApp/app/hire-us/hire-us.component.ts
@@ -1,7 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpWrapper } from '../shared/http/http.wrapper';
+import { CONFIG } from '../config';
+import { NgForm } from '@angular/forms';
+
+//SERVICE
+import { NotificationService } from '../shared/notifications/notification.service';
+
+//MODELS
+import { EmailModel } from '../horse-advertisments/models/emailModel';
 
 @Component({
   templateUrl: './hire-us.component.html'
 })
 
-export class HireUsComponent { }
+export class HireUsComponent implements OnInit {
+  @ViewChild('quoteForm') public quoteForm: NgForm;
+  emailModel: EmailModel = <EmailModel>{};
+  errorMessage: string;
+  notificationRefresh: number;
+
+  private _contactReceiveEmailUrl = CONFIG.baseUrls.apiUrl + "contactformemail";
+
+  constructor(private _httpWrapper: HttpWrapper,
+    private _notificationService: NotificationService) { }
+
+  ngOnInit() {
+    this.notificationRefresh = this._notificationService.getRefresh();
+  }
+
+  send() {
+    this._httpWrapper.post(this._contactReceiveEmailUrl, JSON.stringify(this.emailModel))
+      .subscribe(res => {
+        this.notificationRefresh++;
+        this._notificationService.setRefreshAndText(this.notificationRefresh, this._notificationService.contactEmailSentSuccessText());
+        this.quoteForm.resetForm();
+      },
+      error => this.errorMessage = error);
+  }
+}
