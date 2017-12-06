fad36ca381d3b37f490572d5bec07a3df8636cea
Paul Ionescu <ionepaul76@gmail.com>
2017-12-06 08:26:05 +0200

    latest fixes

--- HorseSpot.Web/ClientApp/app/app.module.ts
+++ HorseSpot.Web/ClientApp/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AdsenseModule } from 'ng2-adsense';
-import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 //MODULES
 import { SharedModule } from './shared/shared.module';
